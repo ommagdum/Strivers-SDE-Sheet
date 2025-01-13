014a1bcd80e12bce8178a8b4315e2ec8665b6c67
Om Magdum <om.college.id@gmail.com>
2025-01-13 15:20:54 +0530

    SOLVED PROBLEM 15

---
+++ Strivers-SDE-Sheet/src/MajorityElement.java
@@ -0,0 +1,30 @@
+
+public class MajorityElement {
+	
+	public static int majorityElement(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        int element = 0;
+        for(int i = 0; i < n; i++){
+            if(count == 0){
+                count = 1;
+                element = nums[i];
+            }else if(nums[i] == element){
+                count++;
+            }else{
+                count--;
+            }
+        }
+
+        return element;
+    }
+	
+	public static void main(String[] args) {
+		
+		int[] nums = {2,2,1,1,1,2,2};
+		System.out.print("Majority Element : "+ majorityElement(nums));
+	
+
+	}
+
+}
