b0ba9af15e4e9e37cc6f38db4f5436d525fee43b
Om Magdum <om.college.id@gmail.com>
2025-01-28 20:25:43 +0530

    SOLEVED PROBLEM 28

---
+++ Strivers-SDE-Sheet/src/RemoveNthNode.java
@@ -0,0 +1,33 @@
+import MergeTwoSortedLinkedLists.ListNode;
+
+class ListNode {
+	      int val;
+	      ListNode next;
+	      ListNode() {}
+	      ListNode(int val) { this.val = val; }
+	      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	  }
+public class RemoveNthNode {
+	
+
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode left = dummy;
+        ListNode right = head;
+
+        while(n>0 && right!=null){
+            right = right.next;
+            n -= 1;
+        }
+
+        while(right != null){
+            left = left.next;
+            right = right.next;
+        }
+
+        left.next = left.next.next;
+        
+        return dummy.next;
+    }
+
+}
