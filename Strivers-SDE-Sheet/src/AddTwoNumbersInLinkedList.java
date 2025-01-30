8d73034dd5604b60b5dd8c4bdd267c9698e68ba1
Om Magdum <om.college.id@gmail.com>
2025-01-30 20:53:17 +0530

    SOLVED PROBLEM 30

--- Strivers-SDE-Sheet/src/AddTwoNumbersInLinkedList.java
+++ Strivers-SDE-Sheet/src/AddTwoNumbersInLinkedList.java
@@ -1,10 +1,4 @@
-class ListNode {
-	      int val;
-	      ListNode next;
-	      ListNode() {}
-	      ListNode(int val) { this.val = val; }
-	      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-	  }
+
 public class AddTwoNumbersInLinkedList {
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode();
---
+++ Strivers-SDE-Sheet/src/DeleteNodeFromLinkedList.java
@@ -0,0 +1,7 @@
+
+public class DeleteNodeFromLinkedList {
+	public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
