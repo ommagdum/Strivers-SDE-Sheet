a98b41fe72b34b19457d3d7a32527218fd68b367
Om Magdum <om.college.id@gmail.com>
2025-02-19 19:58:34 +0530

    SOLVED PROBLEM 42

---
+++ Strivers-SDE-Sheet/src/MaxConsecutiveOnes.java
@@ -0,0 +1,17 @@
+
+public class MaxConsecutiveOnes {
+	public int findMaxConsecutiveOnes(int[] nums) {
+        int cnt = 0;
+        int maxi = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
+
+            maxi = Math.max(maxi, cnt);
+        }
+        return maxi;
+    }
+}
